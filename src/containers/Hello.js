2f034d3c2004892dd4ceb9351881c2d5210664e3
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-03-02 09:51:10 +0900

    state管理のコード追加

--- src/containers/Hello.js
+++ src/containers/Hello.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, TouchableOpacity, View } from 'react-native';
 
 class Hello extends Component {
   static navigationOptions = {
     title: 'Hello',
   };
+
+  constructor(props) {
+    super(props);
+    this.state = { checked: false };
+  }
+
+  onPressCheck = () => this.setState({ checked: !this.state.checked });
+
+  onPressNext = () => this.props.navigation.navigate('World');
+
   render() {
     return (
       <View>
         <Text>Hello!</Text>
-        <Button title="NEXT" onPress={() => this.props.navigation.navigate('World')} />
+        <TouchableOpacity onPress={this.onPressCheck}>
+          <Text>チェックをするとボタンが活性化します</Text>
+        </TouchableOpacity>
+        <Button title="Next" onPress={this.onPressNext} disabled={!this.state.checked} />
       </View>
     );
   }
